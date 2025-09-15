675da2c50903c2e186c73bd7a71fd7bbc0f85c0d
Michael Cunegundes <michael.cunegundes@gmail.com>
2025-09-15 16:17:39 -0300

    mudou

--- src/main/java/com/libras/backend/config/QuizDataInitializer.java
+++ src/main/java/com/libras/backend/config/QuizDataInitializer.java
@@ -44,18 +44,18 @@ public class QuizDataInitializer implements ApplicationRunner {
     private void criarNivel1_CumprimentosBasicos() {
         log.info("👋 Criando Nível 1: Cumprimentos Básicos...");
 
-        // 1. OI (duas partes)
+        // 1. OI
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/oipart1.png,/images/oipart2.png",
-                List.of("Bom dia", "Boa tarde", "Oi", "Tchau"),
+                "/images/nivel1/oi1.png,/images/nivel1/oi12.png",
+                List.of("Bom dia", "Boa tarde", "Oi", "Tudo bem?"),
                 2, 1
         ));
 
         // 2. DESCULPA
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/desculpa.png",
+                "/images/nivel1/desculpa1.png",
                 List.of("Tchau", "Oi", "Obrigado", "Desculpa"),
                 3, 1
         ));
@@ -63,8 +63,8 @@ public class QuizDataInitializer implements ApplicationRunner {
         // 3. TCHAU
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/tchau1.png",
-                List.of("Oi", "Bom dia", "Tchau", "Obrigado"),
+                "/images/nivel1/tchau1.png",
+                List.of("Oi", "Bom noite", "Tchau", "Obrigado"),
                 2, 1
         ));
 
@@ -72,7 +72,7 @@ public class QuizDataInitializer implements ApplicationRunner {
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.TEXTO_PARA_IMAGEM,
                 "Obrigado",
-                List.of("/images/obrigado2.png", "/images/idade.png", "/images/tchau1.png", "/images/bomdia4.png"),
+                List.of("/images/nivel1/obrigado1.png", "/images/idade.png", "/images/tchau1.png", "/images/bomdia4.png"),
                 0, 1
         ));
 
@@ -80,8 +80,8 @@ public class QuizDataInitializer implements ApplicationRunner {
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.TEXTO_PARA_IMAGEM,
                 "Eu",
-                List.of("/images/eu1.png", "/images/ir1.png", "/images/trem1.png", "/images/carro1.png"),
-                0, 1
+                List.of("/images/null/mae.png", "/images/nivel1/eu1.png", "/images/null/trem.png", "/images/null/quasepao1.png"),
+                1, 1
         ));
     }
 
@@ -91,7 +91,7 @@ public class QuizDataInitializer implements ApplicationRunner {
         // 1. BOM DIA (sequência)
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/bomdia1.png,/images/bomdia2.png,/images/bomdia3.png,/images/bomdia4.png",
+                "/images/nivel2/bomdia2.png,/images/nivel2/bomdia22.png,/images/nivel2/bomdia23.png,/images/nivel2/bomdia24.png",
                 List.of("Bom dia", "Tudo bem?", "Prazer em conhecer", "Boa tarde"),
                 0, 2
         ));
@@ -101,32 +101,32 @@ public class QuizDataInitializer implements ApplicationRunner {
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.TEXTO_PARA_IMAGEM,
                 "Carro",
-                List.of("/images/moto1.png", "/images/onibus1.png", "/images/idade.png", "/images/carro1.png"),
+                List.of("/images/nivel2/moto2.png", "/images/nivel2/onibus2.png", "/images/nivel2/trem2.png", "/images/nivel2/carro2.png"),
                 3, 2
         ));
 
         // 3. EU VOU
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/eu1.png,/images/ir1.png",
-                List.of("Eu sou", "Tenho fome", "Eu vou", "Eu e você"),
+                "/images/nivel2/eu2.png,/images/nivel2/ir2.png",
+                List.of("Eu sou", "Estou com fome", "Eu vou", "Eu e você"),
                 2, 2
         ));
 
         // 4. ÔNIBUS
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/onibus1.png,/images/onibus2.png",
+                "/images/nivel2/onibus2.png,/images/nivel2/onibus22.png",
                 List.of("Carro", "Moto", "Ônibus", "Trem"),
                 2, 2
         ));
 
 
-        // 5. CASA
+        // 5. Idade
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.TEXTO_PARA_IMAGEM,
-                "Casa",
-                List.of("/images/gemini1.png", "/images/casa1.png", "/images/moto1.png", "/images/joia1.png"),
+                "Idade",
+                List.of("/images/nivel2/gemini2.png", "/images/nivel2/idade2.png", "/images/nivel2/cinema2.png", "/images/nivel2/cafe2.png"),
                 1, 2
         ));
     }
@@ -138,7 +138,7 @@ public class QuizDataInitializer implements ApplicationRunner {
         // 1. MÃE
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/mae1.png,/images/mae22.png",  // Adicionar esta imagem
+                "/images/nivel3/mae3.png,/images/nivel3/mae32.png",  // Adicionar esta imagem
                 List.of("Pai", "Mãe", "Irmão", "Avó"),
                 1, 3
         ));
@@ -148,7 +148,7 @@ public class QuizDataInitializer implements ApplicationRunner {
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.TEXTO_PARA_IMAGEM,
                 "Tio",
-                List.of("/images/idade.png", "/images/comer1.png", "/images/tio1.png", "/images/quaseirmao1.png"),
+                List.of("/images/null/obrigado.png", "/images/null/joia.png", "/images/nivel3/tio3.png", "/images/null/quaseirmao.png"),
                 2, 3
         ));
 
@@ -157,7 +157,7 @@ public class QuizDataInitializer implements ApplicationRunner {
         // 3. PAI
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/quasepai3.png,/images/mae22.png", // Adicionar esta imagem
+                "/images/nivel3/pai3.png,/images/nivel3/mae32.png", // Adicionar esta imagem
                 List.of("Pai", "Tio", "Avô", "Primo"),
                 0, 3
         ));
@@ -165,7 +165,7 @@ public class QuizDataInitializer implements ApplicationRunner {
         // 4. Amigo
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/amigo.png",  // Adicionar esta imagem
+                "/images/nivel3/amigo3.png",  // Adicionar esta imagem
                 List.of("Primo", "Amigo", "Irmão", "Conhecido"),
                 1, 3
         ));
@@ -173,8 +173,8 @@ public class QuizDataInitializer implements ApplicationRunner {
         // 5. Familia
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/familia1.png,/images/familia2.png",
-                List.of("Irmaos", "Time", "Grupo", "Familia"),
+                "/images/nivel3/familia31.png,/images/nivel3/familia32.png",
+                List.of("Irmãos", "Time", "Grupo", "Familia"),
                 3, 3
         ));
     }
@@ -185,7 +185,7 @@ public class QuizDataInitializer implements ApplicationRunner {
         // 1. ÁGUA
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/agua1.png,/images/agua2.png",  // Adicionar esta imagem
+                "/images/nivel4/agua41.png,/images/nivel4/agua42.png",  // Adicionar esta imagem
                 List.of("Leite", "Café", "Água", "Suco"),
                 2, 4
         ));
@@ -193,7 +193,7 @@ public class QuizDataInitializer implements ApplicationRunner {
         // 2. Restaurante
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/quaserestaurante1.png,/images/quaserestaurante2.png",  // Adicionar esta imagem
+                "/images/nivel4/restaurante41.png,/images/nivel4/restaurante42.png",  // Adicionar esta imagem
                 List.of("Bebida", "Restaurante", "Cozinha", "Jantar"),
                 1, 4
         ));
@@ -201,7 +201,7 @@ public class QuizDataInitializer implements ApplicationRunner {
         // 3. CAFÉ
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/cafe1.png,/images/cafe2.png",  // Adicionar esta imagem
+                "/images/nivel4/cafe41.png,/images/nivel4/cafe42.png",  // Adicionar esta imagem
                 List.of("Chá", "Bebida", "Leite", "Café"),
                 3, 4
         ));
@@ -209,8 +209,8 @@ public class QuizDataInitializer implements ApplicationRunner {
         // 4. Comer
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/comer1.png,/images/comer2.png",  // Adicionar esta imagem
-                List.of("Comer", "Fome", "Venha", "Jantar"),
+                "/images/nivel4/comer41.png,/images/nivel4/comer42.png",  // Adicionar esta imagem
+                List.of("Comer", "Fome", "Vem", "Jantar"),
                 0, 4
         ));
 
@@ -218,7 +218,7 @@ public class QuizDataInitializer implements ApplicationRunner {
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.TEXTO_PARA_IMAGEM,
                 "Arroz",
-                List.of("/images/quasepao1.png", "/images/oipart1.png", "/images/arroz1.png", "/images/mae2.png"),
+                List.of("/images/null/quasepao1.png", "/images/null/shoping.png", "/images/nivel4/arroz4.png", "/images/null/bomdia.png"),
                 2, 4
         ));
     }
@@ -229,7 +229,7 @@ public class QuizDataInitializer implements ApplicationRunner {
         // 1. Casa
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/casa1.png",  // Adicionar esta imagem
+                "/images/nivel5/casa5.png",  // Adicionar esta imagem
                 List.of("Casa", "Escola", "Hospital", "Mercado"),
                 0, 5
         ));
@@ -237,7 +237,7 @@ public class QuizDataInitializer implements ApplicationRunner {
         // 2. HOSPITAL
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/quasehospital1.png,/images/quasehospital2.png",  // Adicionar esta imagem
+                "/images/nivel5/hospital51.png,/images/nivel5/hospital52.png",  // Adicionar esta imagem
                 List.of("Hospital", "Hotel", "Escola", "Banco"),
                 0, 5
         ));
@@ -246,14 +246,14 @@ public class QuizDataInitializer implements ApplicationRunner {
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.TEXTO_PARA_IMAGEM,
                 "Shopping",  // Adicionar esta imagem
-                List.of("/images/idade.png", "/images/onibus1.png", "/images/moto1.png", "/images/shoping.png"),
+                List.of("/images/null/idade.png", "/images/null/trem.png", "/images/null/moto.png", "/images/nivel5/shoping5.png"),
                 3, 5
         ));
 
         // 2. Banheiro
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.IMAGEM_PARA_TEXTO,
-                "/images/quasebanheiro1.png",  // Adicionar esta imagem
+                "/images/nivel5/banheiro5.png",  // Adicionar esta imagem
                 List.of("Hospital", "Hotel", "Banheiro", "Banco"),
                 2, 5
         ));
@@ -262,7 +262,7 @@ public class QuizDataInitializer implements ApplicationRunner {
         perguntaService.salvar(criarPergunta(
                 TipoPergunta.TEXTO_PARA_IMAGEM,
                 "Cinema",
-                List.of("/images/cinema1.png", "/images/onibus2.png", "/images/pai1.png", "/images/quasepai2.png"),
+                List.of("/images/nivel5/cinema5.png", "/images/null/onibus.png", "/images/null/pai.png", "/images/quasepai2.png"),
                 0, 5
         ));
     }
